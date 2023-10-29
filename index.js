21a2f723cc2866949aae9f9053850c37990c0896
Dolon Roy <dolonr718@gmail.com>
2023-10-29 18:26:19 +0600

    bookings product access with email id and delete with id commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(express.json());
 app.use(cors());
@@ -25,12 +25,64 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     const serviceCollection = client.db("carDoctorDB").collection("serviceCollection");
+    const productsCollection = client.db("carDoctorDB").collection("productsCollection");
+    const bookingsCollection = client.db("carDoctorDB").collection("bookingsCollection");
 
     app.get("/services" , async (req, res) => {
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
 
         res.send(result);
+    })
+
+    app.post("/bookings", async (req, res) => {
+      const data = req.body;
+      
+      const result = await bookingsCollection.insertOne(data);
+      res.send(result);
+    })  
+
+    app.get("/bookings",async (req, res) => {
+      const userEmail = req.query.email;
+      const query = {email : userEmail};
+      
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const  query = {_id: new ObjectId(id)};
+
+      const result = await bookingsCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.get("/services/title", async (req, res) => {
+
+      const option = {
+        projection: {title: 1}
+      }
+       
+      const query = {};
+
+      const data = await serviceCollection.find(query, option).toArray();
+      res.send(data);
+    })
+
+    app.get("/services/:id", async (req, res) => {
+      const id = (req.params.id);
+      const query = {_id : id};
+
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
     await client.db("admin").command({ ping: 1 });
@@ -41,7 +93,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
 
 
 app.get('/', async (req, res) => {
